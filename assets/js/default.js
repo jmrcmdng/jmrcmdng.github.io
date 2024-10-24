c2fbf58cb8d0ffdcf90bb9b7c381a158439a8039
jmrcmdng <jmrcmdng@gmail.com>
2024-10-24 20:39:36 +0800

    change to app

---
+++ assets/js/default.js
@@ -0,0 +1,15 @@
+﻿window.setThemePreference = (theme) => {
+    localStorage.setItem('theme', theme);
+};
+
+window.getThemePreference = () => {
+    return localStorage.getItem('theme');
+};
+
+window.getPreferredColorScheme = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
+
+window.openInNewTab = (url) => {
+    window.open(url, '_blank');
+}
